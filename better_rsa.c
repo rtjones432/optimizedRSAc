b7a6b81ff7173d937e181b8c623b7cede5b7de06
Ryan Jones <rtjones432@gmail.com>
2023-11-27 20:20:21 -0500

    fixed encrypt and added a hex test

--- better_rsa.c
+++ better_rsa.c
@@ -5,24 +5,36 @@
 #include <stdlib.h>
 #define block_size 64
 
+void pmenc(mpz_t m, mpz_t e, mpz_t n, mpz_t c) {
+  mpz_out_str(stdout, 10, m);
+  printf("^");
+  mpz_out_str(stdout, 10, e);
+  printf("mod");
+  mpz_out_str(stdout, 10, n);
+  printf("=");
+  mpz_out_str(stdout, 10, c);
+  printf("\n");
+}
+
 // encrypts a single block
 void encrypt_block(mpz_t e, mpz_t m, mpz_t *c, mpz_t n) {
-  mpz_set_ui(*c, 0);
+  mpz_set_ui(*c, 1);
 
   if (mpz_tstbit(e, 0)) {
-    mpz_add(*c, *c, m);
+    mpz_mul(*c, *c, m);
     mpz_mod(*c, *c, n);
   }
-
-  while (e) {
+  pmenc(m, e, n, *c);
+  while (mpz_sgn(e)) {
     mpz_fdiv_q_2exp(e, e, 1);
     mpz_mul(m, m, m);
     mpz_mod(m, m, n);
 
     if (mpz_tstbit(e, 0)) {
-      mpz_add(*c, *c, m);
+      mpz_mul(*c, *c, m);
       mpz_mod(*c, *c, n);
     }
+    pmenc(m, e, n, *c);
   }
 }
 
@@ -40,7 +52,9 @@ void encrypt_hex(mpz_t e, char *message, char *cipher_text, mpz_t n) {
 
 int main(int argc, char *argv[]) {
   mpz_t e, n;
-  char str[10] = "hi";
+  mpz_init_set_ui(e, 7);
+  mpz_init_set_ui(n, 77);
+  char str[10] = "9";
   char ct[10];
   encrypt_hex(e, str, ct, n);
   printf("ct: %s", ct);
