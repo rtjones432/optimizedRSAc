475d2f9bd2a330b01a7b7a4bf0e113da511efcb4
Ryan Jones <rtjones432@gmail.com>
2023-11-29 23:23:14 -0500

    touch up work

--- src/rsa_timing_test.c
+++ src/rsa_timing_test.c
@@ -15,25 +15,26 @@ int main(int argc, char *argv[]) {
   mpz_init(e);
   mpz_init(d);
   mpz_init(n);
-  unsigned long size_list[8] = {16, 32, 64, 128, 256, 512, 1024, 2048};
+  unsigned long size_list[9] = {16, 32, 64, 128, 256, 512, 1024, 2048, 4096};
   gmp_randstate_t state;
   gmp_randinit_default(state);
   for (int i = 0; i < sizeof(size_list) / sizeof(unsigned long); i++) {
-    printf("\ntesting on keysize and block size of %lu\n", size_list[i]);
+    printf("\ntesting on keysize and block size of %lu\n", 2 * size_list[i]);
     start = clock();
     keygen(n, e, d, size_list[i]);
     stop = clock();
     // mpz_out_str(stdout, 10, n);
     // mpz_out_str(stdout, 10, e);
     // mpz_out_str(stdout, 10, d);
-    printf("in ticks %ld\n", stop - start);
+    printf("key generation clocks: %ld time: %lf\n", stop - start,
+           (1.0 * (stop - start)) / CLOCKS_PER_SEC);
     mpz_urandomb(m, state, size_list[i]);
     if (i == 0) {
       start = clock();
       lin_encrypt_block(e, m, c, n);
       stop = clock();
-      printf("loop method clocks: %ld time: %ld\n", stop - start,
-             (stop - start) / CLOCKS_PER_SEC);
+      printf("loop method clocks: %ld time: %lf\n", stop - start,
+             (1.0 * (stop - start)) / CLOCKS_PER_SEC);
     } else {
       printf("loop method clocks: --- time: ---\n");
     }
@@ -41,16 +42,16 @@ int main(int argc, char *argv[]) {
     start = clock();
     log_encrypt_block(e, m, c, n);
     stop = clock();
-    printf("log method clocks %ld time %ld\n", stop - start,
-           (stop - start) / CLOCKS_PER_SEC);
+    printf("log method clocks %ld time %lf\n", stop - start,
+           (1.0 * (stop - start)) / CLOCKS_PER_SEC);
 
     // start timer
     start = clock();
     gmp_encrypt_block(e, m, c, n);
     // stop timer
     stop = clock();
-    printf("gmp method clocks %ld time %ld\n", stop - start,
-           (stop - start) / CLOCKS_PER_SEC);
+    printf("gmp method clocks %ld time %lf\n", stop - start,
+           (1.0 * (stop - start)) / CLOCKS_PER_SEC);
   }
   return EXIT_SUCCESS;
 }
